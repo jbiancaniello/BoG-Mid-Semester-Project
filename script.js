9d64c66846aaff2ace0e1a8955a88c1a8c5e4927
jbiancaniello <76523489+jbiancaniello@users.noreply.github.com>
2022-10-06 19:25:08 -0400

    Next Button Functionality Added
    Next Button Functionality Added

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const apiURL = "https://pokeapi.co/api/v2/pokemon/";
-let id = 3;
+let id = 1;
 
 async function getPokemon() {
     const response = await fetch(apiURL + id);
@@ -11,7 +11,13 @@ async function getPokemon() {
 document.getElementById("prevButton").addEventListener("click", () => {
     if (id > 1 || id < 905) { //fix this
         id--;
-        document.getElementById("prevButton").textContent = id;
+        getPokemon();
+    }
+});
+
+document.getElementById("nextButton").addEventListener("click", () => {
+    if (id > 1 || id < 905) { //fix this
+        id++;
         getPokemon();
     }
 });
