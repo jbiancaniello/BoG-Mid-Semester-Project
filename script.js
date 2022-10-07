5e332d82560781dbf0e20cd4ff3bfba11ee736f1
jbiancaniello <76523489+jbiancaniello@users.noreply.github.com>
2022-10-07 15:44:06 -0400

    Logic & Types
    Fixed logic bug, got types to display

--- script.js
+++ script.js
@@ -4,28 +4,39 @@ let id = 1;
 async function getPokemon() {
     const response = await fetch(apiURL + id);
     const data = await response.json();
-    getPhoto();
+    setPhoto();
     setName(data);
+    setTypes(data);
 }
 
 document.getElementById("prevButton").addEventListener("click", () => {
-    if (id > 1 & id < 905) {
-        id--;
+    id--;
+    if (id >= 1 & id < 905) {
         getPokemon();
+    } else {
+        id++;
     }
 });
 
 document.getElementById("nextButton").addEventListener("click", () => {
-    if (id > 1 & id < 905) {
+    if (id >= 1 & id < 905) {
         id++;
         getPokemon();
     }
 });
 
-function getPhoto() {
+function setPhoto() {
     document.getElementById("photo").src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
 }
 
 function setName(data) {
     document.getElementById("textBox").textContent = data.name;
+}
+
+function setTypes(data) {
+    let curr = "";
+    for (let i = 0; i < data.types.length; i++) {
+        curr = curr + " " + data.types[i].type.name;
+    }
+    document.getElementById("types").textContent = curr;
 }
